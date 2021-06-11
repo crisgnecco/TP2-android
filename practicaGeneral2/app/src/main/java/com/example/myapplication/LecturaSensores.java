d5b78c94a7f11d41fe88c7f8d6c5073a20672e20
MaxiAlexander <maxialexander36@gmail.com>
2021-06-11 16:57:51 -0300

    Shared Preference para la temperatura y detecta si el movil esta horizontal

--- practicaGeneral2/app/src/main/java/com/example/myapplication/LecturaSensores.java
+++ practicaGeneral2/app/src/main/java/com/example/myapplication/LecturaSensores.java
@@ -12,49 +12,36 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.DecimalFormat;
 
 public class LecturaSensores extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
-    private TextView acelerometro;
-    private TextView temperatura;
-    private TextView detectaHorizontal;
-    DecimalFormat dosdecimales = new DecimalFormat("###.###");
+    private EditText temperatura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        acelerometro = findViewById(R.id.acelerometro);
         temperatura = findViewById(R.id.temperatura);
-        detectaHorizontal = findViewById(R.id.detectaHorizontal);
-
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        cargarValoresSensores();
+        cargarTemperatura();
 
     }
 
     private void registrarSensores(){
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),   SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),   SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     private void quitarSensores(){
         mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
-        mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT));
     }
 
-    public void iniciarSenseo(View view){
-        registrarSensores();
-    }
-
-    public void finalizarSenseo(View view){
-        quitarSensores();
-    }
 
     @Override
     protected void onPause() {
@@ -65,12 +52,12 @@ public class LecturaSensores extends AppCompatActivity implements SensorEventLis
     @Override
     protected void onResume() {
         super.onResume();
-        //registrarSensores();
+        registrarSensores();
     }
 
     @Override
     protected void onRestart() {
-        // registrarSensores();
+        registrarSensores();
         super.onRestart();
     }
 
@@ -82,7 +69,6 @@ public class LecturaSensores extends AppCompatActivity implements SensorEventLis
 
     @Override
     protected void onDestroy() {
-        guardarValoresSensores();
         quitarSensores();
         super.onDestroy();
     }
@@ -90,44 +76,20 @@ public class LecturaSensores extends AppCompatActivity implements SensorEventLis
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        String texto = "";
 
         synchronized (this)
         {
-            Log.d("sensor", event.sensor.getName());
+            Log.d("Acelerometro", String.valueOf(event.values[0]));
 
             switch(event.sensor.getType())
             {
 
                 case Sensor.TYPE_ACCELEROMETER :
-                    texto += "Acelerometro:\n";
-                    texto += "x: " + dosdecimales.format(event.values[0]) + " m/seg2 \n";
-                    texto += "y: " + dosdecimales.format(event.values[1]) + " m/seg2 \n";
-                    texto += "z: " + dosdecimales.format(event.values[2]) + " m/seg2 \n";
-                    acelerometro.setText(texto);
 
-                    if(event.values[0] > 9.5 || event.values[0] < -9.5){
-                        detectaHorizontal.setBackgroundColor(Color.RED);
-                        detectaHorizontal.setText("Celular en horizontal");
-                    }else{
-                        detectaHorizontal.setBackgroundColor(Color.parseColor("#FAFAFA"));
-                        detectaHorizontal.setText("");
-                    }
-
-                    /*
                     if(event.values[0] > 9.5 || event.values[0] < -9.5)
                         Toast.makeText(getApplicationContext(), "Celular en horizontal", Toast.LENGTH_SHORT).show();
-                    */
 
                     break;
-
-                case Sensor.TYPE_LIGHT :
-                    texto += "Temperatura\n";
-                    texto += event.values[0] + " °C \n";
-
-                    temperatura.setText(texto);
-                    break;
-
             }
         }
 
@@ -138,32 +100,33 @@ public class LecturaSensores extends AppCompatActivity implements SensorEventLis
 
     }
 
-    private void cargarValoresSensores(){
-        SharedPreferences preferences = getSharedPreferences("valoresSensores", Context.MODE_PRIVATE);
+    private void cargarTemperatura(){
+        SharedPreferences preferences = getSharedPreferences("temperatura", Context.MODE_PRIVATE);
 
-        String acelerometro = preferences.getString("acelerometro", "No hay valores anteriores");
-        String proximidad = preferences.getString("temperatura", "No hay valores anteriores");
+        String temperatura = preferences.getString("temperatura", "");
 
-        this.acelerometro.setText(acelerometro);
-        this.temperatura.setText(proximidad);
+        this.temperatura.setText(temperatura);
     }
 
-    private void guardarValoresSensores(){
+    private void guardarTemperatura(){
 
-        SharedPreferences preferences = getSharedPreferences("valoresSensores", Context.MODE_PRIVATE);
+        SharedPreferences preferences = getSharedPreferences("temperatura", Context.MODE_PRIVATE);
 
-        String acelerometro = this.acelerometro.getText().toString();
-        String proximidad = this.temperatura.getText().toString();
+        String temperatura = this.temperatura.getText().toString();
 
         SharedPreferences.Editor editor = preferences.edit();
 
-        editor.putString("acelerometro", acelerometro);
-        editor.putString("temperatura", proximidad);
-
-        System.out.println(acelerometro);
-        System.out.println(proximidad);
+        editor.putString("temperatura", temperatura);
 
         editor.apply();
+
+    }
+
+    public void procesarFomulario(View view){
+
+        guardarTemperatura();
+
+        Toast.makeText(getApplicationContext(), "Gracias por llenar el formulario", Toast.LENGTH_SHORT).show();
 
     }
 }
